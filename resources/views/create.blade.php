e54440b245b0c35f0b853bb7bd31b40d8368dcec
tamai <kzzhh07@gmail.com>
2022-07-13 22:27:00 +0900

    メモ保存機能追加

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card">
         <div class="card-header">新規メモ作成</div>
-        <form class="card-body" action="/store" method="POST">
+        <form class="card-body" action="{{ route{'store'} }}" method="POST">
             @csrf
             <div class="form-group">
                 <textarea class="form-control" name="content" placeholder="ここにメモを入力" rows="3"></textarea>
