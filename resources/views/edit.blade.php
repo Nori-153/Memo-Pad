91279dff0792834c591f3414e4352e6b997bf169
tamai <kzzhh07@gmail.com>
2022-07-15 08:16:27 +0900

    メモ編集画面修正

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">メモ編集</div>
+        <form class="card-body" action="{{ route('store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <textarea class="form-control" name="content" placeholder="ここにメモを入力" rows="3">{{ $edit_memo['content'] }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">更新</button>
+        </form>
+    </div>
+@endsection
